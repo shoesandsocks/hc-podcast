c2600ec7ce333997e68d85bd480fa0f3013fe691
RL Brown <rlbrown72@gmail.com>
2020-03-03 16:05:41 -0500

    changeup in progress

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const markdownIt = require("markdown-it");
+// const markdownIt = require("markdown-it");
 const CleanCSS = require("clean-css");
-
+const { DateTime } = require("luxon")
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addFilter("debug", obj => console.log(obj));
@@ -14,6 +14,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("limit", function(arr, num) {
     return arr.slice(0, num);
   });
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  })
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.setTemplateFormats(["md", "njk", "html"]);
   eleventyConfig.addPassthroughCopy("assets");
